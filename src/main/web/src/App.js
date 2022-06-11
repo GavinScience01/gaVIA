4a89e81f06e07c67aaf0bcfcbe5645c64c1c939a
GavinScience01 <gavinscience.awesome@gmail.com>
2022-06-11 19:14:56 -0400

    Added full navbar functionality courtesy of Fireship, added basic arrowless keyboard layout

--- src/main/web/src/App.js
+++ src/main/web/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { ReactComponent as KeyboardIcon } from './icons/keyboard.svg';
-import { ReactComponent as MouseIcon } from './icons/mouse.svg';
-import { ReactComponent as LightIcon } from './icons/light.svg';
+import React, {useState} from 'react';
+import {CSSTransition} from 'react-transition-group';
 
 function App() {
     return (
         <NavBar>
-            <NavItem icon="⌨️"></NavItem>
+            <NavItem icon="⌨️">
+                <DropdownMenu></DropdownMenu>
+            </NavItem>
             <NavItem icon="🖱️"></NavItem>
             <NavItem icon="🚦"></NavItem>
         </NavBar>
@@ -21,12 +21,60 @@ function NavBar(props) {
     );
 }
 
+function DropdownMenu() {
+
+    const [activeMenu, setActiveMenu] = useState('main');
+    const [menuHeight, setMenuHeight] = useState(null);
+
+    function calcHeight(el) {
+        const height = el.offsetHeight;
+        setMenuHeight(height);
+    }
+    
+    function DropDownItem(props) {
+        return (
+            <a href='#' className='menu-item' onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
+                <span className='icon-button'>{props.leftIcon}</span>
+                {props.children}
+                <span className='icon-right'>{props.rightIcon}</span>
+            </a>
+        );
+    }
+
+    return (
+        <div className='dropdown' style={{height: menuHeight}}>
+            <CSSTransition in={activeMenu == 'main'} unmountOnExit timeout={500} classNames='menu-primary' onEnter={calcHeight}>
+                <div className='menu'>
+                    <DropDownItem>80%</DropDownItem>
+                    <DropDownItem>75%</DropDownItem>
+                    <DropDownItem goToMenu="60%" rightIcon="🡺">60%</DropDownItem>
+                    <DropDownItem>40%</DropDownItem>
+                    <DropDownItem>20%</DropDownItem>
+                </div>
+            </CSSTransition>
+
+            <CSSTransition in={activeMenu == '60%'} unmountOnExit timeout={500} classNames='menu-secondary' onEnter={calcHeight}>
+                <div className='menu'>
+                    <DropDownItem leftIcon="🡸" goToMenu="main" />
+                    <DropDownItem>Arrowed</DropDownItem>
+                    <DropDownItem>Arrowless</DropDownItem>
+                </div>
+            </CSSTransition>
+        </div>
+    );
+}
+
 function NavItem(props) {
+
+    const [open, setOpen] = useState(false);
+
     return (
         <li className='nav-item'>
-            <a href='#' className='icon-button'>
+            <a href='#' className='icon-button' onClick={() => setOpen(!open)}>
                 {props.icon}
             </a>
+
+            {open && props.children}
         </li>
     );
 }
